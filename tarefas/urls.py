a31b85b048d69eb9bd551e1943e83bde7c7bf1bc
JGiotech <giovannabarbosah38@gmail.comm>
2025-06-24 19:31:13 -0300

    Atualizando

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tarefas_list, name='tarefas_list'),           # nome para a lista
+    path('', views.tarefas_list, name='tarefas_list'),          
    path('add/', views.adicionar_tarefas, name='adicionar_tarefas'),
-     # nome para adicionar
+   
     path('edit/<int:pk>/', views.editar_tarefas, name='editar_tarefas'),
     path('delete/<int:pk>/', views.deletar_tarefas, name='deletar_tarefas'),
 ]
